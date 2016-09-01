b1c56be3ca4a9014b45d13a97f9f6856229cfc3b
gusanthiago <gusanthiagodv@gmail.com>
2016-09-01 00:24:18 -0300

    adicionando dependencias

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/gusanthiago/projects/farmacia_rubens/farmaciasocial/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
